11be0776e1fcdcbc661f04eeb6d0ef0809d8a447
yafitrizauhair <ytrizauhair@gmail.com>
2025-11-11 21:47:56 +0700

    code google scriptnya

---
+++ src/pages/googleSciprt.js
@@ -0,0 +1,54 @@
+function doPost(e) {
+  try {
+  
+    const data = JSON.parse(e.postData.contents);
+
+    const sheet = SpreadsheetApp.openById("ganti id anda di sini")
+      .getSheetByName("Sheet1");
+
+    sheet.appendRow([
+      new Date(),
+      data.name || "",
+      data.email || "",
+      data.subject || "",
+      data.message || "",
+    ]);
+
+
+    const result = {
+      status: "success",
+      message: "Pesan berhasil dikirim!",
+    };
+
+    return ContentService
+      .createTextOutput(JSON.stringify(result))
+      .setMimeType(ContentService.MimeType.JSON);
+  } catch (err) {
+    const errorResult = {
+      status: "error",
+      message: err.message,
+    };
+
+    return ContentService
+      .createTextOutput(JSON.stringify(errorResult))
+      .setMimeType(ContentService.MimeType.JSON);
+  }
+}
+
+
+function doOptions() {
+  return ContentService
+    .createTextOutput("")
+    .setMimeType(ContentService.MimeType.TEXT)
+    .setHeader("Access-Control-Allow-Origin", "*")
+    .setHeader("Access-Control-Allow-Methods", "POST, OPTIONS")
+    .setHeader("Access-Control-Allow-Headers", "Content-Type");
+}
+
+
+function doGet() {
+  return ContentService
+    .createTextOutput("Contact API aktif")
+    .setMimeType(ContentService.MimeType.TEXT)
+    .setHeader("Access-Control-Allow-Origin", "*");
+}
